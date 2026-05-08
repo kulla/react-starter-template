e05dac17ee6d04e080fa779d8f4fcda987a2898b
Stephan Kulla <git.mail@kulla.me>
2026-05-08 15:21:02 +0200

    refactor: Remove "page" CSS
    Closes https://github.com/kulla/rsbuild-react-starter-template/issues/103

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <main className="page">
+    <main>
       <h1>Bun with React</h1>
       <p>Start building amazing things with Bun.</p>
     </main>
